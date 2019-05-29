04b75cfc56e3867dc2217b7922d44038cfaacb83
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-29 15:26:01 -0400

    Changed so that loop ends with break insted of return

--- exercise.py
+++ exercise.py
@@ -7,6 +7,7 @@ seating_plan = [
 
 
 def display_available_seats(list):
+    done = False
     for row in range(0, len(seating_plan)):
         for seat in range(0, len(seating_plan[row])):
             if seating_plan[row][seat] == None:
@@ -15,11 +16,29 @@ def display_available_seats(list):
                 )
                 user_answer = input()
                 if user_answer == "y":
+                    done = True
                     print("What is your name?")
                     user_name = input()
                     seating_plan[row][seat] = user_name
-                    return
+                    break
+                if done:
+                    break
+            if done:
+                break
 
 
 display_available_seats(seating_plan)
 print(seating_plan)
+
+# Row 1 Seat 1 is available - Do you want to sit there y/n?
+# n
+# Row 1 Seat 3 is available - Do you want to sit there y/n?
+# y
+# What is your name?
+# Jess
+# [
+#     [None, "Pumpkin", "Jess", None],
+#     ["Socks", None, "Mimi", None],
+#     ["Gismo", "Shadow", None, None],
+#     ["Smokey", "Toast", "Pacha", "Mau"],
+# ]
